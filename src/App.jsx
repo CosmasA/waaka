880ccf2cabd564f3a1574123d8d56aa37c6c2568
CosmasA <cosmasapuke@gmail.com>
2025-01-22 17:37:24 +0300

    Modified About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import NavBar from "./components/Home/Navbar";
 import About from "./components/About/About";
 import Footer from "./components/Home/Footer";
 import Home from "./components/Home/Home";
+import ScrollToTop from "./components/Home/ScrollToTop";
 
 function App() {
   return (
     <Router basename="/waaka">
       <div className="app">
+        <ScrollToTop />
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -1,60 +1,77 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { FaUser, FaShoppingCart } from "react-icons/fa";
 import Logo from "../../assets/logo.png";
 
 const NavBar = () => {
   const userName = "Apuke Cosmas";
 
+  // State to manage the navbar collapse
+  const [expanded, setExpanded] = useState(false);
+
+  // Close the navbar when a link is clicked
+  const handleLinkClick = () => {
+    setExpanded(false); // Collapse the navbar
+  };
+
   return (
-    <>
-      {/* Navbar */}
-      <Navbar expand="lg" className="navbar fixed-top">
-        <Container>
-          <Navbar.Brand as={Link} to="/" className="navbar-brand">
-            <img className="logo" src={Logo} alt="app_logo" />
-            Plotus
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about">
-                About
-              </Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-              <Nav.Link href="#property">Properties</Nav.Link>
+    <Navbar
+      expand="lg"
+      className="navbar fixed-top"
+      expanded={expanded} // Control expanded state
+      onToggle={() => setExpanded(!expanded)} // Toggle on button click
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/" className="navbar-brand">
+          <img className="logo" src={Logo} alt="app_logo" />
+          Plotus
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" onClick={handleLinkClick}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" onClick={handleLinkClick}>
+              About
+            </Nav.Link>
+            <Nav.Link href="#contact" onClick={handleLinkClick}>
+              Contact
+            </Nav.Link>
+            <Nav.Link href="#property" onClick={handleLinkClick}>
+              Properties
+            </Nav.Link>
 
-              {/* User Profile Section */}
-              <NavDropdown
-                title={
-                  <span className="profile">
-                    <FaUser className="fa-user" /> {userName}
-                  </span>
-                }
-                id="user-profile-dropdown"
-              >
-                <NavDropdown.Item href="#profile">
-                  View Profile
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#settings">Settings</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
-              </NavDropdown>
+            {/* User Profile Section */}
+            <NavDropdown
+              title={
+                <span className="profile">
+                  <FaUser className="fa-user" /> {userName}
+                </span>
+              }
+              id="user-profile-dropdown"
+            >
+              <NavDropdown.Item href="#profile" onClick={handleLinkClick}>
+                View Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#settings" onClick={handleLinkClick}>
+                Settings
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#logout" onClick={handleLinkClick}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
 
-              {/* Cart Section */}
-              <Nav.Link href="#cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+            {/* Cart Section */}
+            <Nav.Link href="#cart" onClick={handleLinkClick}>
+              <FaShoppingCart /> Cart
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
